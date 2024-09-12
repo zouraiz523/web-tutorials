45f952870ea822d65d0ab8a622e1fc459e1924ba
Zouraiz <84671958+zouraiz523@users.noreply.github.com>
2024-09-12 11:58:45 +0500

    Add files via upload

---
+++ Light Dark Mode Page/script.js
@@ -0,0 +1,10 @@
+$( ".inner-switch" ).on("click", function() {
+    if( $( "body" ).hasClass( "dark" )) {
+      $( "body" ).removeClass( "dark" );
+      $( ".inner-switch" ).text( "OFF" );
+    } 
+    else {
+      $( "body" ).addClass( "dark" );
+      $( ".inner-switch" ).text( "ON" );
+    }
+  });
