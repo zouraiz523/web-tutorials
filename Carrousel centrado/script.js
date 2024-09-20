ce601c61663f3ed833cd6f4ed32693f8ffe956c4
Zouraiz <84671958+zouraiz523@users.noreply.github.com>
2024-09-20 14:46:40 +0500

    Add files via upload

---
+++ Carrousel centrado/script.js
@@ -0,0 +1,27 @@
+var carousel = $(".carousel"),
+    items = $(".item"),
+    currdeg  = 0;
+
+$(".next").on("click", { d: "n" }, rotate);
+$(".prev").on("click", { d: "p" }, rotate);
+
+function rotate(e){
+  if(e.data.d=="n"){
+    currdeg = currdeg - 60;
+  }
+  if(e.data.d=="p"){
+    currdeg = currdeg + 60;
+  }
+  carousel.css({
+    "-webkit-transform": "rotateY("+currdeg+"deg)",
+    "-moz-transform": "rotateY("+currdeg+"deg)",
+    "-o-transform": "rotateY("+currdeg+"deg)",
+    "transform": "rotateY("+currdeg+"deg)"
+  });
+    items.css({
+    "-webkit-transform": "rotateY("+(-currdeg)+"deg)",
+    "-moz-transform": "rotateY("+(-currdeg)+"deg)",
+    "-o-transform": "rotateY("+(-currdeg)+"deg)",
+    "transform": "rotateY("+(-currdeg)+"deg)"
+  });
+}
