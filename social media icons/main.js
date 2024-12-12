20dd098febb88601c6b76673b3d143ec5ebbb623
Zouraiz <84671958+zouraiz523@users.noreply.github.com>
2024-12-12 11:39:35 +0500

    Add files via upload

---
+++ social media icons/main.js
@@ -0,0 +1,10 @@
+    // VanillaTilt uygulanması
+    VanillaTilt.init(document.querySelectorAll(".sci li a"), {
+        max: 30,
+        speed: 400,
+        glare: true,
+        "max-glare": 0.5
+    });
+
+    // Arka plan rengini değiştirme
+  
