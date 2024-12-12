96f6e9ea8c8f0ce66199a705c9a6831d8cc69575
Zouraiz <84671958+zouraiz523@users.noreply.github.com>
2024-12-12 13:29:48 +0500

    Update main.js

--- social media icons/main.js
+++ social media icons/main.js
@@ -7,4 +7,15 @@
     });
 
     // Arka plan rengini değiştirme
-  
+    let list = document.querySelectorAll(".sci li");
+    let bg = document.querySelector("body");
+
+    list.forEach((element) => {
+        element.addEventListener("mouseover", function(event) {
+            let color = element.style.getPropertyValue("--clr");
+            bg.style.backgroundColor = color;
+        });
+        element.addEventListener("mouseleave", function() {
+            bg.style.backgroundColor = '#fff';
+        });
+    });
